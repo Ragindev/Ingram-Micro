e8ed9fe7d0ccd240477bbf9c46623e6259bc4577
Ramees Muhammed <rameesmuhammed957@gmail.com>
2022-06-21 21:18:30 +0530

    collect order form bc and show in browser using flask

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-# Get Order from BigCommerce
+from flask import Flask, render_template 
 import requests
 
-url = "https://api.bigcommerce.com/stores/b5ajmj9rbq/v2/orders/111"
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    url = "https://api.bigcommerce.com/stores/b5ajmj9rbq/v2/orders/111"
+    header = {
+        "X-Auth-Token":"redptv84kmlgfed97l7jroa0mdknfgc",
+        "Content-Type":"application/json",
+        "Accept": "application/json"
+    }
+    response = requests.request("GET",url,headers=header)
+    print(response.text)
+    return render_template('/index.html',response=response)
 
 
-header = {
-    "X-Auth-Token":"redptv84kmlgfed97l7jroa0mdknfgc",
-    "Content-Type":"application/json",
-    "Accept": "application/json"
-}
-
-response = requests.request("GET",url,headers=header)
-print(response.text)
+if __name__ == ('__main__'):
+    app.run(debug=True)
