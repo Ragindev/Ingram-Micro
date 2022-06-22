27e2ef9e5886ab994d3b12559bc2e740738de465
Ramees Muhammed <rameesmuhammed957@gmail.com>
2022-06-22 18:38:53 +0530

    webhook test and product test

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+from urllib import response
 from flask import Flask, render_template 
 import requests
 
@@ -12,10 +14,24 @@ def index():
         "Content-Type":"application/json",
         "Accept": "application/json"
     }
-    response = requests.request("GET",url,headers=header)
-    print(response.text)
+    response = requests.request("GET",url,headers=header).json()
+    # print(response)
+    # print("Customer ID is : ",response['customer_id'])  24233840
     return render_template('/index.html',response=response)
 
+@app.route('/product', methods=["GET", "POST"])
+def product():
+    url = "https://api.bigcommerce.com/stores/b5ajmj9rbq/v3/hooks"
+
+    headers = {
+        "Content-Type": "",
+        "Accept": "",
+        "X-Auth-Token": "redptv84kmlgfed97l7jroa0mdknfgc"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+    print(response.text)
+    return render_template('/index.html', response=response)
 
 if __name__ == ('__main__'):
     app.run(debug=True)
