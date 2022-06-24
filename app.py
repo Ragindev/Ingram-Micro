52b405b3ccd5173c98ed66b404e2ae8a2fee3966
Ramees Muhammed <rameesmuhammed957@gmail.com>
2022-06-24 18:32:45 +0530

    get response from webhook and using id get product detsils

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import http
 import json
+from math import prod
 from pickle import GET
 from urllib3 import request
 from flask import Flask, render_template,request
@@ -8,28 +9,41 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    url = "https://api.bigcommerce.com/stores/b5ajmj9rbq/v2/orders/111"
+@app.route('/', methods=["GET", "POST"])
+def product():
     header = {
         "X-Auth-Token":"redptv84kmlgfed97l7jroa0mdknfgc",
         "Content-Type":"application/json",
         "Accept": "application/json"
     }
-    response = requests.request("GET",url,headers=header).json()
-    # print(response)
-    # print("Customer ID is : ",response['customer_id'])  24233840
-    return render_template('/index.html',response=response)
-
-@app.route('/product', methods=["GET", "POST"])
-def product():
+    productdata = ''
     if request.method =='POST':
-        print('Product Updated',request.json)  #Get all data 
-        return 'New Changes'
+        productdata = request.json
+        productID = productdata['data']['id']
+        print('Product Updated  :',request.json)  #Get all data 
+        print ("=================")
+        print ("Product DATA : ",productdata['data']['id'])
+        print ("=================")
+        print (productID)
 
     elif request.method =='GET':    
         print('New Request')
-        return 'New Request'
-    return render_template('/product.html')
+        # newRequest = 'New REQUEST'
+        # return newRequest
+    print("****** product data *********")
+    print(productdata)
+    print("****** product ID IS  *********")
+    print(productdata['data']['id'])
+    print("****** product ID IS  *********")
+    product_id = productdata['data']['id']
+    print("****** product ID new var is  *********")
+    print (product_id)
+    url = f"https://api.bigcommerce.com/stores/b5ajmj9rbq/v3/catalog/products/{product_id}"
+    
+    response = requests.request("GET",url,headers=header).json()
+    # print("Customer ID is : ",response['customer_id'])
+    print("RESPONSE is : ",response)
+    return render_template('/index.html', header=header, response=response,productdata=productdata)
+
 if __name__ == ('__main__'):
     app.run(debug=True)
