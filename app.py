f4d61838ea1e90465d2c57c97a2a218d809d7cad
Ramees Muhammed <rameesmuhammed957@gmail.com>
2022-06-25 16:30:22 +0530

    1. find order id 2. find the number and  product ID

--- app.py
+++ app.py
@@ -15,41 +15,53 @@ def product():
         "X-Auth-Token":"redptv84kmlgfed97l7jroa0mdknfgc",
         "Content-Type":"application/json",
         "Accept": "application/json"
+        # STORE HASH : b5ajmj9rbq
     }
-    productdata = ''
+    order_data = ''
     if request.method =='POST':
-        productdata = request.json
-        productID = productdata['data']['id']
-        print('Product Updated  :',request.json)  #Get all data 
-        print ("=================")
-        print ("Product DATA : ",productdata['data']['id'])
-        print ("=================")
-        print (productID)
+        order_data = request.json
+        order_id = order_data['data']['id']
+        # print('ORDER DETAILS  :',request.json)  #Get all data 
+        print("=================")
+        print("ORDER ID : ",order_data['data']['id'])
+        
+        print("======= GET Full ORDER Details ==========")
+        print(order_data)
 
     elif request.method =='GET':    
         print('New Request')
         newRequest = 'New REQUEST'
         return newRequest
         
-    product_id = productdata['data']['id']
-    store_id = productdata['store_id']
-    store_hash = productdata['producer']
-    
-    print("****** Product DATA  *********")
-    print(productdata)
+    order_id = order_data['data']['id']
+    store_hash = order_data['producer']
 
     print("****** Product ID IS  *********")
-    print(product_id)
+    print(order_id)
     
-    print("****** STORE ID IS  *********")
-    print(store_id)
+    print("****** STORE HAS IS  *********")
+    print(store_hash)
     
-    url = f"https://api.bigcommerce.com/{store_hash}/v3/catalog/products/{product_id}"
+    # url = f"https://api.bigcommerce.com/{store_hash}/v3/catalog/products/{product_id}"
+    # url = f"https://api.bigcommerce.com/{store_hash}/v2/orders/{order_id}"
+    url = f"https://api.bigcommerce.com/{store_hash}/v2/orders/{order_id}/products"
     
     response = requests.request("GET",url,headers=header).json()
-    print("***********  Product Name *********** ")
-    print(response['data']['name'])
-    return render_template('/index.html', header=header, response=response,productdata=productdata)
+    # print("***********  Product Name *********** ")
+    print("**** FIND THE INDEX LENGTH *****")
+    print("NUMBER OF LENGTH  :",  len(response))
+    
+    print("**** FIND THE PRODUCT *****")
+    print(response[0])
+    
+    print("**** FIND THE PRODUCT *****")
+    print("product_id  :  ",response[0]['product_id'])
+    
+    for i in response:
+        print("****** ALL PRODUCTS ID ARE *******")
+        print("product_id",i["product_id"])
+    
+    return render_template('/index.html', header=header, order_data=order_data, response=response)
 
 if __name__ == ('__main__'):
     app.run(debug=True) 
