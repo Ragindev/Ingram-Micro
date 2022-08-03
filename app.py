83494ae0e1c187592fa9a6158ac4641fe7db36dc
Ramees Muhammed <rameesmuhammed957@gmail.com>
2022-08-03 13:12:02 +0530

    created order in ingrammicro using the data from bigcommerce

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def processWebhookPayload(order_data):
     # print(order_data)
 ########################################################
 ########################################################
-    
+
     print("********** GET ORDER DATA FROM WEBHOOK *********** \n")
     store_hash = order_data['producer']
     scope = order_data['scope']
@@ -95,11 +95,71 @@ def processWebhookPayload(order_data):
     # print('\n', shipping_address, '\n')
     print("****** GET THE SHIPPING DETAILS USING THE API  ******* \n")
     pp.pprint(shipping_address)
-    
+    createOrder(shipping_address)
     print('\n')
     print('\n')
     return "got it" , 200
-    
+
+
+########################################################
+########################################################
+
+@app.route('/createOrder')
+def createOrder(shipping_address):
+    #Header Used to access ingrammicro
+    headers = {
+    'accept': 'application/json',
+    'IM-CustomerNumber': '280695',
+    'IM-CountryCode': 'AU',
+    'IM-SenderID': 'IngramMicro',
+    'IM-CorrelationID': '2022-07-29T05:31:04+0000',
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer NbtKEEUbgPGYTrF7p4HkSXEkHGow'
+    }
+    #API to create a order in ingrammicro 
+    url = "https://api.ingrammicro.com:443/sandbox/resellers/v6/orders"
+    payload = json.dumps({
+    "customerOrderNumber": shipping_address[0]['order_id'],
+    "endCustomerOrderNumber": "ENDUSERPO1",
+    "additionalAttributes": [
+        {
+        "attributeName": "allowPartialOrder",
+        "attributeValue": "false"
+        }
+    ],
+    "shipToInfo": {
+        "contact": shipping_address[0]['first_name'],
+        "companyName": shipping_address[0]['company'],
+        "addressLine1": shipping_address[0]['street_1'],
+        "addressLine2": shipping_address[0]['street_2'],
+        "city":  shipping_address[0]['city'],   
+        "state": "SA",  #Should be the state of AU
+        "postalCode": "4209", #4 Digit Postal Code in AU   
+        "countryCode": shipping_address[0]['country_iso2'],  
+        "phoneNumber":  shipping_address[0]['phone'],
+        "email":  shipping_address[0]['email']
+    },
+    "lines": [
+        {
+        "customerLineNumber": "001",
+        "ingramPartNumber": "2985452",
+        "quantity": 1,
+        "unitPrice": 58.41
+        },
+        {
+        "customerLineNumber": "002",
+        "ingramPartNumber": "2985452",
+        "vendorPartNumber": "",
+        "quantity": 1,
+        "unitPrice": 220.51
+        }
+    ]
+    })
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print("****** CREATE A ORDER USING THE ABOVE DATA  ******* \n")
+    print(response.text)
+    # return "Test Function "
+
 if __name__ == '__main__':
     app.run()
 
