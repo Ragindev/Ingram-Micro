2e75e370b864806ef1693395cdac2de350829751
Ramees Muhammed <rameesmuhammed957@gmail.com>
2022-08-04 12:36:09 +0530

    Generate new token for every order

--- app.py
+++ app.py
@@ -21,18 +21,18 @@ def listern():
     backgroundThread.start()
     return 'Yeah, I got it',200
 
-# @app.route('/try-your-self',methods=["GET"])
-# def tryYourSelf():
-#     orderdata = {
-#         'producer':'stores/b5ajmj9rbq',
-#         'scope':'store/cart/converted',
-#         'store_id':'1001802518',
-#         'data':{
-#             'orderId':'317'
-#         }
-#     }
-#     processWebhookPayload(orderdata)
-#     return 'Yeah, I got it'
+@app.route('/try-your-self',methods=["GET"])
+def tryYourSelf():
+    orderdata = {
+        'producer':'stores/b5ajmj9rbq',
+        'scope':'store/cart/converted',
+        'store_id':'1001802518',
+        'data':{
+            'orderId':'317'
+        }
+    }
+    processWebhookPayload(orderdata)
+    return 'Yeah, I got it'
 
 def processWebhookPayload(order_data):
     header = {
@@ -41,7 +41,6 @@ def processWebhookPayload(order_data):
         "Accept": "application/json"
         # STORE HASH : b5ajmj9rbq
     }
-    print("1. Funnction is working  \n ")
     
     # print(order_data)
 ########################################################
@@ -92,10 +91,11 @@ def processWebhookPayload(order_data):
     #  Collecting shiping Address data from order shipping_address API  
     url = f"https://api.bigcommerce.com/{store_hash}/v2/orders/{order_id}/shipping_addresses"
     shipping_address = requests.request("GET",url,headers=header).json()
-    # print('\n', shipping_address, '\n')
+    
     print("****** GET THE SHIPPING DETAILS USING THE API  ******* \n")
+    
     pp.pprint(shipping_address)
-    createOrder(shipping_address)
+    createOrder(shipping_address) # Calling the Create Order Function 
     print('\n')
     print('\n')
     return "got it" , 200
@@ -106,18 +106,20 @@ def processWebhookPayload(order_data):
 
 @app.route('/createOrder')
 def createOrder(shipping_address):
-    # create token
+    # Create token for every new order 
+    client_id = 'vxGA45MVwXFEWYvjzITSGTdGAgeygbct'
+    client_secret = 'FyW7sATOWbGKFrOW'
     url = "https://api.ingrammicro.com:443/oauth/oauth30/token"
-    payload='grant_type=client_credentials&client_id=vxGA45MVwXFEWYvjzITSGTdGAgeygbct&client_secret=FyW7sATOWbGKFrOW'
+    payload=f'grant_type=client_credentials&client_id={client_id}&client_secret={client_secret}'
     headers = {
     'Content-Type': 'application/x-www-form-urlencoded'
     }
-    response = requests.request("POST", url, headers=headers, data=payload)
-    print(response.text)
+    tokenResponse = requests.request("POST", url, headers=headers, data=payload)
+    # print(json.loads(tokenResponse.text))
+    # convert the tokenResponse from string to Dictionary using (json.loads)
+    accessToken =json.loads(tokenResponse.text)
+    
     print("*********** token created **********")
-    
-    
-    #Header Used to access ingrammicro
     headers = {
     'accept': 'application/json',
     'IM-CustomerNumber': '280695',
@@ -125,9 +127,9 @@ def createOrder(shipping_address):
     'IM-SenderID': 'IngramMicro',
     'IM-CorrelationID': '2022-07-29T05:31:04+0000',
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer tRDEIaefR4BLsAGKbHqZJLxbAz71' 
+    'Authorization': 'Bearer '+ accessToken['access_token']  # Passing token to the header 
     }
-    #API to create a order in ingrammicro 
+    # API to create a order in ingrammicro 
     url = "https://api.ingrammicro.com:443/sandbox/resellers/v6/orders"
     payload = json.dumps({
     "customerOrderNumber": shipping_address[0]['order_id'],
@@ -145,9 +147,9 @@ def createOrder(shipping_address):
         "addressLine2": shipping_address[0]['street_2'],
         "city":  shipping_address[0]['city'],   
         "state": "SA",  #Should be the state of AU
-        "postalCode": "4209", #4 Digit Postal Code in AU   
-        "countryCode": shipping_address[0]['country_iso2'],  
-        "phoneNumber":  shipping_address[0]['phone'],
+        "postalCode": "4209", #4 Digit Postal Code available in AU   
+        "countryCode": shipping_address[0]['country_iso2'], #must be AU  
+        "phoneNumber":  shipping_address[0]['phone'], 
         "email":  shipping_address[0]['email']
     },
     "lines": [
@@ -169,7 +171,7 @@ def createOrder(shipping_address):
     response = requests.request("POST", url, headers=headers, data=payload)
     print("****** CREATE A ORDER USING THE ABOVE DATA  ******* \n")
     print(response.text)
-    # return "Test Function "
+    return "Order Created"
 
 if __name__ == '__main__':
     app.run()
